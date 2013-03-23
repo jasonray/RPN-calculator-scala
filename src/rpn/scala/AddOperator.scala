b4131bb10a4eb4cfd4d217b4eb02a2a90428188e
Jason Ray <jasonray@jayray.net>
2013-03-22 22:42:25 -0400

    moved add to binary operator

--- src/rpn/scala/AddOperator.scala
+++ src/rpn/scala/AddOperator.scala
@@ -1,11 +1,8 @@
 package rpn.scala
 
-class AddOperator extends Operator {
-  def performOperation(numbers: RpnStack): Int = {
-    val rhs = numbers.pop
-    val lhs = numbers.pop
-    val result = lhs + rhs
-    numbers.push(result)
-    return result
+class AddOperator extends BinaryOperator {
+  def doOperation(lhs: Int, rhs: Int): Int = {
+    lhs + rhs;
   }
+
 }
---
+++ src/rpn/scala/BinaryOperator.scala
@@ -0,0 +1,13 @@
+package rpn.scala
+
+abstract class BinaryOperator extends Operator {
+  def performOperation(numbers: RpnStack): Int = {
+    val rhs = numbers.pop
+    val lhs = numbers.pop
+    val result = doOperation(lhs, rhs);
+    numbers.push(result)
+    return result
+  }
+
+  def doOperation(lhs: Int, rhs: Int): Int;
+}
