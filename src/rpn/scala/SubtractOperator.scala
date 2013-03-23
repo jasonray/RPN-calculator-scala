9b751bf54a763f84d6e4e06afee8203ce75be81a
Jason Ray <jasonray@jayray.net>
2013-03-22 22:44:04 -0400

    added subtraction to binary operator

--- src/rpn/scala/SubtractOperator.scala
+++ src/rpn/scala/SubtractOperator.scala
@@ -1,11 +1,8 @@
 package rpn.scala
 
-class SubtractOperator extends Operator {
-  def performOperation(numbers: RpnStack): Int = {
-    val rhs = numbers.pop
-    val lhs = numbers.pop
-    val result = lhs - rhs
-    numbers.push(result)
-    return result
+class SubtractOperator extends BinaryOperator {
+  def doOperation(lhs: Int, rhs: Int): Int = {
+    lhs - rhs;
   }
+
 }
